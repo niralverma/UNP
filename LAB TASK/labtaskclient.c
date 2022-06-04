0af3f5dcf700b18d69b49e557f7047ed0f0b481f
Niral Verma <76837066+niralverma@users.noreply.github.com>
2022-06-04 09:42:15 +0530

    Update labtaskclient.c

--- LAB TASK/labtaskclient.c
+++ LAB TASK/labtaskclient.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include<stdlib.h>
 #include<signal.h>
-int net_socket;
+int net_socket; //socketfd
 int status;
 void handle_sigint(int sig)
 {
@@ -32,6 +32,19 @@ int main(int argc, char *argv[])
     server_address.sin_port = htons(atoi(argv[2]));
     server_address.sin_addr.s_addr = inet_addr(argv[1]);
 
+
+
+    /*
+     * syntax of connect:
+     * int connect(int socket, const struct sockaddr * address, socklen_t adress_len);
+     * input :  int socket : file descriptor of socket
+     *          const char struct sockaddr *address : pointer to variable of sockaddr type
+     *          socklen_t : size of sockaddr
+     * description: three way handshake connect function makes an attempt to make a connection on a socket
+     * return : 0 if successful else -1
+     * **/
+     
+     
     status=connect( net_socket, ( struct sockaddr * ) &server_address, sizeof( server_address));
     if(status==-1)
     {
@@ -49,11 +62,11 @@ while(1){
       write(net_socket,data, strlen(data));
       
  n=recv(net_socket, data,200,0);
-    printf("Received from client %d %s port number  %d  and ip: %s:%d\n",n,data,server_address.sin_port,inet_ntoa(server_address.sin_addr));
+    printf("Received from server %d %s port number  %d  and ip: %s\n",n,data,server_address.sin_port,inet_ntoa(server_address.sin_addr));
     
 
       n=recv(net_socket, data,200,0);
-       printf("Received from client %d %s port number  %d  and ip: %s:%d\n",n,data,server_address.sin_port,inet_ntoa(server_address.sin_addr));
+       printf("Received from server %d %s port number  %d  and ip: %s\n",n,data,server_address.sin_port,inet_ntoa(server_address.sin_addr));
     
       write(net_socket,data, strlen(data));
 
