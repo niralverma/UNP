c4366afcb94eb83b48f1fe11950e5c688f16ec75
Niral Verma <76837066+niralverma@users.noreply.github.com>
2022-06-25 09:50:15 +0530

    Update iomux.c

--- IO MUX/iomux.c
+++ IO MUX/iomux.c
@@ -79,4 +79,67 @@ int main()
 output: 0
         
  /////////////////////////////////////////////////////////////
+       POLL
        
+     #include<sys/select.h>
+#include<stdio.h>
+#include<sys/time.h>
+#include<poll.h>
+//struct timeval{
+//long tv_sec;
+//long tv_usec;
+//};
+int main()
+{  /*
+        struct timeval t;
+        t.tv_sec=5;
+        fd_set read_set;
+        FD_ZERO(&read_set);
+        FD_SET(0,&read_set); //FD_SET(fileno(stdin),&read_set);
+        int sr = select(1,&read_set,NULL,NULL,&t);
+        
+
+        printf("Completed...with select return %d\n",sr);
+*/
+        struct pollfd fds[1];
+        fds[0].fd=0;
+        fds[0].events=POLLIN|POLLERR;
+        poll(fds,1,-1);
+
+        printf("completed...\n");
+        return(0);
+
+}
+
+/////////////////////////////////////////////////////////////////
+
+#include<sys/select.h>
+#include<stdio.h>
+#include<sys/time.h>
+#include<poll.h>
+//struct timeval{
+//long tv_sec;
+//long tv_usec;
+//};
+int main()
+{  /*
+        struct timeval t;
+        t.tv_sec=5;
+        fd_set read_set;
+        FD_ZERO(&read_set);
+        FD_SET(0,&read_set); //FD_SET(fileno(stdin),&read_set);
+        int sr = select(1,&read_set,NULL,NULL,&t);
+        
+
+        printf("Completed...with select return %d\n",sr);
+*/
+        struct pollfd fds[1];
+        fds[0].fd=0;
+        fds[0].events=POLLIN|POLLERR;
+        poll(fds,1,5000); //5000ms to wait
+
+        printf("completed...\n");
+        return(0);
+
+}
+
