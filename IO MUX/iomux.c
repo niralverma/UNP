c7c7103ba070f55454ce93e256feccd88a06d884
Niral Verma <76837066+niralverma@users.noreply.github.com>
2022-06-25 09:18:36 +0530

    Update iomux.c

--- IO MUX/iomux.c
+++ IO MUX/iomux.c
@@ -28,3 +28,26 @@ int main()
         return(0);
 
 }
+
+output: -1
+
+////////////////////////////////////////////////////
+
+
+
+#include<sys/select.h>
+#include<stdio.h>
+int main()
+{
+        fd_set read_set;
+        FD_ZERO(&read_set);
+        FD_SET(0,&read_set); //FD_SET(fileno(stdin),&read_set);
+        int sr = select(1,&read_set,NULL,NULL,NULL);
+
+
+        printf("Completed...with select return %d\n",sr);
+        return(0);
+
+}
+
+output: 1
