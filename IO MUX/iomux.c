24e27e376056ee4dd7bbe7ffb030c23d5338edef
Niral Verma <76837066+niralverma@users.noreply.github.com>
2022-06-25 09:16:59 +0530

    Update iomux.c

--- IO MUX/iomux.c
+++ IO MUX/iomux.c
@@ -12,3 +12,19 @@ int main()
 }
 
 //blocking mode
+
+
+#include<sys/select.h>
+#include<stdio.h>
+int main()
+{
+        fd_set read_set;
+        FD_ZERO(&read_set);
+        FD_SET(3,&read_set); //FD_SET(fileno(stdin),&read_set);
+        int sr = select(4,&read_set,NULL,NULL,NULL);
+
+
+        printf("Completed...with select return %d\n",sr);
+        return(0);
+
+}
